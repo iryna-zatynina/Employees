17ee25fca3b2148741fe3ea7fcdfb68e130c2a9d
iryna-zatynina <ira.zat1997@gmail.com>
2022-10-11 10:38:22 +0200

    add JSX

---
+++ src/components/app/app.js
@@ -0,0 +1,43 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import "./app.css";
+
+function App() {
+
+    const data = [
+        {
+            name: 'Jahn C.',
+            salary: '800',
+            increase: true,
+        },
+        {
+            name: 'Alex M.',
+            salary: '3000',
+            increase: true,
+        },
+        {
+            name: 'Carl W.',
+            salary: '5000',
+            increase: false,
+        },
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+            <EmployeesList data={data} />
+            <EmployeesAddForm />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import "./search-panel.css"
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text" 
+            className="form-control seach-input"
+            placeholder="найти сотрудника" />
+    )
+}
+
+export default SearchPanel;
